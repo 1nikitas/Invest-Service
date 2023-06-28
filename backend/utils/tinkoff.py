6c8cf5cd4cc5b0a6ec186a42fc698ca2c1d5d25f
truemoron <totsamyylexa@gmail.com>
2023-06-28 19:25:05 +0300

    [Invest-Service]
    - добавлена структура проекта

---
+++ backend/utils/tinkoff.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from tinkoff.invest import Client, PositionsResponse
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+def main():
+    with Client(TOKEN) as client:
+        account = client.users.get_accounts().accounts[0].id
+        positions: PositionsResponse = client.operations.get_portfolio(account_id=account)
+        print(positions.positions)
+
+if __name__ == "__main__":
+    main()
